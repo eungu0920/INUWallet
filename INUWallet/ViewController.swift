dbfd0096d377b65c495c2d12a77b1d07e00c2847
Gray <eungu0920@inu.ac.kr>
2023-02-01 22:47:19 +0900

    Initial Commit

---
+++ INUWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INUWallet
+//
+//  Created by Gray on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
